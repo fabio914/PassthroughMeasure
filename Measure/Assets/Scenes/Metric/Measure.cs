0177201b28787e71f10c9451f1234d271f751396
Fabio de Albuquerque Dela Antonio <fabio914@gmail.com>
2021-12-23 14:45:27 -0300

    First version

---
+++ Measure/Assets/Scenes/Metric/Measure.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Measure : MonoBehaviour {
+    public Transform leftController;
+    public Transform rightController;
+
+    public GameObject cylinderReference;
+    private List<GameObject> cylinders;
+
+    private GameObject cylinderCopy;
+    private Vector3 initialPoint;
+    private bool isLeft;
+
+    void Start() {
+        cylinders = new List<GameObject>();
+    }
+
+    void Update() {
+        if (cylinderCopy == null) {
+            if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch)) {
+                foreach(GameObject go in cylinders) {
+                    Destroy(go);
+                }
+
+                cylinders.Clear();
+                return;
+            }
+
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch)) {
+                initialPoint = leftController.position;
+                cylinderCopy = Instantiate<GameObject>(cylinderReference);
+                cylinders.Add(cylinderCopy);
+                isLeft = true;
+            } else if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch)) {
+                initialPoint = rightController.position;
+                cylinderCopy = Instantiate<GameObject>(cylinderReference);
+                cylinders.Add(cylinderCopy);
+                isLeft = false;
+            }
+        } else {
+            if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch)) {
+                Destroy(cylinderCopy);
+                cylinderCopy = null;
+                return;
+            }
+
+            if (isLeft) {
+                UpdateCylinder(cylinderCopy, initialPoint, leftController.position);
+
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch)) {
+                    cylinderCopy = null;
+                }
+            } else {
+                UpdateCylinder(cylinderCopy, initialPoint, rightController.position);
+
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch)) {
+                    cylinderCopy = null;
+                }
+            }
+        }
+    }
+
+    private void UpdateCylinder(GameObject reference, Vector3 initial, Vector3 final) {
+        float distance = Vector3.Distance(initial, final);
+
+        foreach (Transform child in reference.transform) {
+            if (child.name == "CylinderParent") {
+                Vector3 newChildScale = child.localScale;
+                newChildScale.z = distance/2.0f;
+                child.transform.localScale = newChildScale;
+
+                child.transform.position = initial;
+                child.transform.LookAt(final);
+            } else if (child.name == "Canvas") {
+                child.transform.position = initial + (final - initial)/2.0f;
+                child.transform.LookAt(final);
+                child.transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);
+                child.transform.Translate(0.0f, 0.075f, 0.0f, Space.Self);
+
+                UnityEngine.UI.Text label = child.gameObject.GetComponentInChildren<UnityEngine.UI.Text>();
+                label.text = $"{(int)System.Math.Floor(distance * 100.0f)} cm";
+            }
+        }
+
+        reference.SetActive(true);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# PassthroughMeasure
-Use your Oculus Quest with Passthrough as a tape measure
+# Passthrough Measure
+
+Use your Oculus Quest 2 with Passthrough as a tape measure. Keep in mind that this is a **very early prototype**.
+
+## Screenshots
+
+<img src="Images/1.jpg" width="300" />
+
+<img src="Images/2.jpg" width="300" />
+
+<img src="Images/3.jpg" width="300" />
+
+## Requirements
+
+- Oculus Quest 2 (system version 35)
+- [SideQuest](https://sidequestvr.com)
+
+## How to install?
+
+- Follow [these instructions](https://sidequestvr.com/setup-howto) to install SideQuest and setup your Oculus Quest 2 for development.
+
+- Download the APK.
+
+- Connect your Oculus Quest to your computer, launch SideQuest and drag the APK to the top left corner of SideQuest to install it.
+
+## How to use it?
+
+- On your Quest 2: open "Apps", select "Unknown Sources" on the drop-down menu on the top right corner, and then select "Measure".
+
+- Press the trigger button to start measuring, and then press it a second time to stop measuring or press "B" (on the right controller) to cancel this measurement.
+
+- Press "B" (on the right controller) to clear all measurements.
+
+## How to build?
+
+- Make sure you have Unity 2020.3 LTS installed (with Android Build support).
+
+- Clone this project.
+
+- Open the project with Unity, then open the Package Manager, and import the Oculus Integration package (version 35.0).
+
+- Navigate to **File > Build Settings...**, select the **Android** platform, then select your Oculus Quest as the **Run device** (if it's plugged in) and then click on **Build and Run**.
+
+## TO-DOs
+
+- [] Add support for imperial units.
+
+- [] Show instructions.
+
+- [] Allow the user to remove a specific measurement.
+
+- [] Add option to save and export all measurements.
